1aea1ef624fd94284017211387d12e65cf670967
d-bev <dmbevi29@gmail.com>
2024-04-12 17:59:28 -0700

    Lunch Break #1

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+PROBLEM PART #2:
+
+Design a webpage to display the calculated metrics from Part 1.
+
+You are free to use any technology in your toolbox to implement this (Pronto mostly uses React)
+Since the number of unique name isn't fixed, display the top 10 sorted by maximum 'measurement', descending. 
+Display all the key metrics as explained in Part 1, along with the number of datapoints that each name has been calculated over.
+
+BONUS PROBLEM:
+
+Visualize a name as its Latitude Longitude Heading change. 
+    - Animate a sprite as these values change 
+    - Design and implementation completely up to you. 
+    - Not doing this portion is not a penalty in anyway, but be prepared to explain approaches that you might take to implement this.
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+ReactDOM.render(
+  <React>
+
+  </React>,
+  document.getElementById("root")
+);
--- sub.py
+++ sub.py
@@ -1,7 +1,32 @@
+"""
+PROBLEM PART #1:
+
+Write a backend storage solution to ingest the data coming from the SERVER. 
+
+The message arrives in the form of:
+    <string: name>;<float: latitude>;<float: longitude>;<float: heading degrees>;<float: measurement>;<string: verification id>
+    
+    Each value of float is at most 999 with only one fractional digit. 
+    String values are at most length of 6. 
+    It is not readily known how many unique name will be published at any point - do not make assumptions that this number is constant.
+
+Key metrics are (per name): the mean, min, max of the measurement and the last known (Latitude, Longitude, and Heading).
+    - You are free to write this in any language that you are comfortable with (Pronto mostly writes in Python and Javascript).
+    - For mean, the value only needs to be accurate to one fractional digit.
+
+    
+PLAN:
+    - create a connection to the SERVER via ZMQ
+    - parse the incoming data
+    - organize the parsed data by which unique 'name it belongs to
+    - calculate the required statistics for each unique 'name'
+    - prepare (and deliver) the calculated statistics for the front-end
+"""
 import time
 import zmq
 import json
 
+DEBUG = False
 
 def main():
 
@@ -13,26 +38,34 @@ def main():
     socket.connect(SERVER)
     socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
+    num_errors = 0
+
     while True:
         try:
             # flags=NOBLOCK raises ZMQError if no messages have arrived
             raw_msg = socket.recv_string(zmq.NOBLOCK)
 
             """
-            The message arrives in the form of:
-                <string: name>;<float: latitude>;<float: longitude>;<float: heading degrees>;<float: measurement>;<string: verification id>
+            I can split each string into its parameters
+            I can group by unique name (or maybe even by verification ID), 
 
-            I feel like I want this data to be portable, so I'm going to split it (using a semicolon as the delimiter), and create JSON objects out of each message
+            But how am I actually going to create the groups of data?
             """
             
-            split_msg = raw_msg.split(';')
+            #TODO: convert to JSON or XML?
 
-            #TODO: convert to JSON?
+            #split_msg = raw_msg.split(';')
 
             # time.sleep(0.5)
             # print(split_msg)
+            
+            print(raw_msg)
+
         except zmq.ZMQError as err:
             print("socket.recv_string() Error: " + str(err))
+
+            # If connection is poor, sleep so we aren't spamming the terminal with errors
+            time.sleep(0.1)
 
 
 
